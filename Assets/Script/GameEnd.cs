09d4adeece3e731a2c47b115ec2d5965a8f6f08f
shariful islam <sisagor98@gmail.com>
2019-11-17 12:53:43 +0600

    replika and pc build

---
+++ Assets/Script/GameEnd.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnd : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Map"))
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
